28ba7dee5eb33c5884c7fc36ca13e359b7725019
xiaogege <sch12635@gmail.com>
2022-02-28 21:53:02 +0900

    Initial commit 게시판 프로젝트 생성

---
+++ src/main/java/com/bluna/bluna/BlunaApplication.java
@@ -0,0 +1,13 @@
+package com.bluna.bluna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlunaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluna/bluna/BlunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluna.bluna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
