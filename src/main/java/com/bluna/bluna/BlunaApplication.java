e5952d42144e23b962ad737e6dba0ed217748e7c
xiaogege <sch12635@gmail.com>
2022-02-28 22:39:45 +0900

    [개발환경셋팅] 스프링시큐리티 비활성화

--- src/main/java/com/bluna/bluna/BlunaApplication.java
+++ src/main/java/com/bluna/bluna/BlunaApplication.java
@@ -2,8 +2,9 @@ package com.bluna.bluna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BlunaApplication {
 
     public static void main(String[] args) {
