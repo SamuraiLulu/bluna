7460c0777cf2d497f89b0daa714c425ab5b1ecd5
kjk <sch12635@gmail.com>
2022-03-03 08:44:38 +0900

    [로그인] 로그인 controller 추가 및 test 추가

---
+++ src/test/java/com/bluna/bluna/controller/LoginControllerTest.java
@@ -0,0 +1,24 @@
+package com.bluna.bluna.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class LoginControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩 (){
+        //when
+        String body = this.restTemplate.getForObject("/login", String.class);
+        //then
+        assertThat(body).contains("login.jsp");
+   }
+}
+
